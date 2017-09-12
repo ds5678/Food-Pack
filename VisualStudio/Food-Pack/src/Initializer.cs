91e2c4cbc7e887ed5eb99fb1d71ed8d885acb433
WulfMarius <WulfMarius@users.noreply.github.com>
2017-09-12 19:17:40 +0200

    added "Aurora Energy Drink"

---
+++ VisualStudio/Food-Pack/src/Initializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AssetLoader;
+using ModComponentMapper;
+using UnityEngine;
+
+namespace FoodPack
+{
+    class Initializer
+    {
+        public static void OnLoad() {
+            ModAssetBundleManager.RegisterAssetBundle("food-pack.unity3d");
+
+            GameObject energyDrinkPrefab = (GameObject)ModAssetBundleManager.LoadAsset("GEAR_AuroraEnergyDrink");
+            ModUtils.RegisterConsoleGearName("AuroraEnergyDrink", energyDrinkPrefab.name);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableBackPack, energyDrinkPrefab, 5);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableKitchenCupboard, energyDrinkPrefab, 3);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableFridge, energyDrinkPrefab, 5);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTablePlasticBox, energyDrinkPrefab, 2);
+        }
+    }
+}
