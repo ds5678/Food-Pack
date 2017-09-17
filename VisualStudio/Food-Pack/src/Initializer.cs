f79b667742fcd5ad483dbcde346dde3c70be2606
WulfMarius <WulfMarius@users.noreply.github.com>
2017-09-17 13:15:35 +0200

    - added cornflakes - added beer

--- VisualStudio/Food-Pack/src/Initializer.cs
+++ VisualStudio/Food-Pack/src/Initializer.cs
@@ -11,15 +11,48 @@ namespace FoodPack
 {
     class Initializer
     {
-        public static void OnLoad() {
-            ModAssetBundleManager.RegisterAssetBundle("food-pack.unity3d");
+        public static void OnLoad()
+        {
+            ModAssetBundleManager.RegisterAssetBundle("food-pack/food-pack.unity3d");
+            ModSoundBankManager.RegisterSoundBank("food-pack/food-pack.bnk");
 
+            initEnergyDrink();
+            initCornflakes();
+            initBeerBottle();
+        }
+
+        private static void initEnergyDrink()
+        {
             GameObject energyDrinkPrefab = (GameObject)ModAssetBundleManager.LoadAsset("GEAR_AuroraEnergyDrink");
             ModUtils.RegisterConsoleGearName("AuroraEnergyDrink", energyDrinkPrefab.name);
             ModUtils.InsertIntoLootTable(LootTableName.LootTableBackPack, energyDrinkPrefab, 5);
             ModUtils.InsertIntoLootTable(LootTableName.LootTableKitchenCupboard, energyDrinkPrefab, 3);
             ModUtils.InsertIntoLootTable(LootTableName.LootTableFridge, energyDrinkPrefab, 5);
             ModUtils.InsertIntoLootTable(LootTableName.LootTablePlasticBox, energyDrinkPrefab, 2);
+        }
+
+        private static void initCornflakes()
+        {
+            GameObject cornFlakesPrefab = (GameObject)ModAssetBundleManager.LoadAsset("GEAR_Cornflakes");
+            ModUtils.RegisterConsoleGearName("Cornflakes", cornFlakesPrefab.name);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableKitchenCupboard, cornFlakesPrefab, 10);
+
+            GearSpawnInfo spawnInfo = new GearSpawnInfo();
+            spawnInfo.PrefabName = cornFlakesPrefab.name;
+            spawnInfo.Position = new Vector3(3.525756f, 0.8793671f, 6.741885f);
+            spawnInfo.Rotation = new Quaternion(0f, 0.7610618f, 0f, 0.6486793f);
+            spawnInfo.SpawnChance = 0.8f;
+            GearSpawner.AddGearSpawnInfo("FarmHouseA", spawnInfo);
+        }
+
+        private static void initBeerBottle()
+        {
+            GameObject BeerBottlePrefab = (GameObject)ModAssetBundleManager.LoadAsset("GEAR_BeerBottle");
+            ModUtils.RegisterConsoleGearName("Beer", BeerBottlePrefab.name);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableBackPack, BeerBottlePrefab, 3);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableKitchenCupboard, BeerBottlePrefab, 5);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableFridge, BeerBottlePrefab, 10);
+            ModUtils.InsertIntoLootTable(LootTableName.LootTableWardrobe_regular, BeerBottlePrefab, 2);
         }
     }
 }
